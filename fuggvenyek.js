ef8f2704205c4edc3c60f2848f749e41eb3b9523
AlexandraLukacs <lszandika15@gmail.com>
2024-04-25 09:22:41 +0200

    index,fuggvenyek alakitas

--- fuggvenyek.js
+++ fuggvenyek.js
@@ -1,16 +1,18 @@
 export function kartyaOsszeallit(lista){
     let txt = "";
-    txt += `<div class="card" style="width:400px">`;
+    txt += `<div class="row 6">`;
     lista.forEach((element, i) => {
-        txt += `<div><img class="card-img-left" src="${element.kep}" alt="Card image"></div>`;
+        txt += `<div class="card" style="width:400px">`;
+        txt += `<div><img class="card-img-left" src="${element.kep}" alt="Card image" style="width:50%"></div>`;
         txt += `<div class="card-body-right">`;
         txt += `<h4 class="card-title">${element.nev}</h4>`;
         txt += `<p class="card-text">${element.ar}Ft</p>`;
         txt += `<p class="card-text">Kategória: ${element.kategoria}</p>`;
         txt += `</div>`;
         txt += `<button type="button" class="btn btn-default">Kosárba</button>`;
+        txt += "</div>";
     });
-    txt += "</div>";
+    txt += "</div>"
     return txt;
 }
 
